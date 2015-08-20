5f2edef31e2dba08cff4042efccd750ae370428d
Ricardo Garcia <ricardo95.garcia@gmail.com>
2015-08-20 18:18:13 -0500

    actualizada con funcion q calcula la long de una lista

--- practica1.rkt
+++ practica1.rkt
@@ -5,4 +5,13 @@
     [(zero? m) 1]
     [else (* n (pow n (- m 1)))]))
 
-    
+
+;Funcion auxiliar
+;mlength que calcula
+;el tamaño de una lista de manera
+;Recursiva .
+
+(define(mlength l)
+  (cond
+    [(empty? l) 0]
+    [else(+ 1 (mlength (cdr l)))]))
