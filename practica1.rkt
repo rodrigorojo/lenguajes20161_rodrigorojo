eccd45f19971d1190dab53e782210c9c852fe0c3
JuanCarlos2 <juancarloslopez0607@gmail.com>
2015-08-22 08:14:59 -0500

    hice suma auxiliar y borre sus pruebas

--- practica1.rkt
+++ practica1.rkt
@@ -1,6 +1,6 @@
 #lang plai
 
-;Funcion auxiliar que eleva un numero n a una potencia m usando recurcion
+;Funcion auxiliar que eleva un numero n a una potencia m usando recurcion.
 
 (define (pow n m)
   (cond
@@ -8,34 +8,31 @@
     [else (* n (pow n (- m 1)))]))
 
 
-;Funcion auxiliar mlength que calculael tamaño de una lista de manera Recursiva .
+;Funcion auxiliar mlength que calcula el tamaño de una lista de manera Recursiva.
 
 (define(mlength l)
   (cond
     [(empty? l) 0]
     [else(+ 1 (mlength (cdr l)))]))
 
-;Funcion que calcula la suma de elementos de una lista no vacia
-;Supondremos que los elementos son enteros.
+;Funcion Average: Dada una lista de enteros, calcula el promedio de sus elementos.
 
-(define (suma lst)
+(define (average l)
+  (define (suma lst)
   (cond
     [(empty? lst) 0]
     [else (+ (car lst) (suma (cdr lst)))]))
-
-;Funcion Average: Dada una lista de enteros, calcula el promedio de sus elementos
-(define (average l)
   (cond
     [(empty? l) 0]
     [else(/ (suma l)(mlength l))]))
-;Función Zip:
+
+;Función Zip, dadas dos listas, regresa una lista cuyos elementos son listas de tamaño dos
+
 (define (zip l1 l2)
   (cond
   [(empty? l1) '()]
   [(empty? l2) '()]
   [else (cons (list (car l1) (car l2)) (zip (cdr l1) (cdr l2)))]))
-
-;Función auxiliar que genera una lista con los dos elementos dados
 
 ;Funcion mconcat: Concatena dos listas recibidas
 
@@ -44,11 +41,12 @@
   [(empty? l) m]
   [(empty? m) l]
   [else (cons (car l) (mconcat (cdr l) m))]))
-;Funcion mmap: Dados una funcion y una lista, aplica la funcion a los elementos
-;de la lista.
+
+;Funcion mmap: Dados una funcion y una lista, aplica la funcion a los elementos de la lista.
 ;Casos: Si la lista es vacía, devolvemos la lista vacía
 ;Sino, definimos la lista de la funcion aplicada a la cabeza, con la llamada recursiva
 ;sobre el resto de la lista
+
 (define (mmap funcion lista)
   (cond 
     [(empty? lista) '()]
@@ -92,17 +90,6 @@
 (test (zip '() '(6 9)) '())
 (test (zip '(7 5 3) '(9 6 4)) '((7 9) (5 6) (3 4)))
 (test (zip '(1 5 3) '(3 4)) '((1 3) (5 4)))
-;Puerbas para funcion auxiliares
-;junta
-;(test (junta 1 3) '(1 3))
-;(test (junta 7 8) '(7 8))
-;(test (junta 3 2) '(3 2))
-;Suma
-(test (suma '(1 2 3 4 5)) 15)
-(test (suma '(1 1 1)) 3)
-(test (suma '(15 6)) 21)
-(test (suma '(1)) 1)
-(test (suma '(1 2 3 4 5 6 7 8 9 10)) 55)
 
 ;Pruebas mconcat
 (test (mconcat '(1 3 4) '()) '(1 3 4))
