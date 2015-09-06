62f401d6466dae0c641030d7b642e71fafe4eea3
Ricardo Garcia <ricardo95.garcia@gmail.com>
2015-09-06 13:29:44 -0500

    agregado concatml y sus respectivas pruebas

--- Practicas/Practica2/practica2.rkt
+++ Practicas/Practica2/practica2.rkt
@@ -28,18 +28,31 @@
   [Square  (p 2D-Point?) (l number?)]
   [Rectangle (p 2D-Point?) (b number?) (h number?)])
 
-;LengthML
+;LengthML- Calcula la longitud de 1 MList
 
 (define (lengthML mlst)
   (cond
   [(MEmpty? mlst) 0]
-  [else (+ 1 (lengthML (MCons-l mlst)))]))
+  [else (+ 1 (lengthML (MCons-l mlst)))])) ;Con "-" haces referencia a esa seccion del constructor (en este caso el resto de mlst)
+
+;concatML- Concatena 2 MList
+(define (concatML ls m)
+  (cond
+  [(MEmpty? ls) m]
+  [(MEmpty? m) ls]
+  [else (MCons (MCons-n ls)(concatML (MCons-l ls) m))]))
 
 
 ;Test
-
+;LengthML
 (test (lengthML (MEmpty)) 0)
 (test (lengthML (MCons 7 (MCons 4 (MEmpty)))) 2)
 (test (lengthML (MCons 7 (MCons 4 (MCons 3 (MEmpty))))) 3)
 (test (lengthML (MCons 7 (MCons 4 (MCons 3 (MCons 4(MEmpty)))))) 4)
 (test (lengthML (MCons 7 (MCons 4 (MCons 3 (MCons 4 (MCons 5(MEmpty))))))) 5)
+;concatML
+(test (concatML (MEmpty)(MCons 3 (MEmpty))) (MCons 3 (MEmpty)))
+(test (concatML (MCons 3 (MEmpty)) (MEmpty)) (MCons 3 (MEmpty)))
+(test (concatML (MCons 7 (MCons 4 (MEmpty))) (MCons 1 (MEmpty))) (MCons 7 (MCons 4 (MCons 1 (MEmpty)))))
+(test (concatML (MCons 1(MEmpty)) (MCons 2(MEmpty))) (MCons 1(MCons 2(MEmpty))))
+(test (concatML (MCons 10(MEmpty)) (MCons 20(MCons 30(MEmpty)))) (MCons 10(MCons 20(MCons 30(MEmpty)))))
