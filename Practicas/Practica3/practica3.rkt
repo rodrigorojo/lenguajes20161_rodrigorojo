7d4b02e9711c8b92744bada541ea89498da60ff6
JuanCarlos2 <juancarloslopez0607@gmail.com>
2015-09-13 00:32:09 -0500

    seccion 2 practica 3 y algunas pruebas

--- Practicas/Practica3/practica3.rkt
+++ Practicas/Practica3/practica3.rkt
@@ -3,6 +3,9 @@
 (print-only-errors true)
 
 (require "practica3-base.rkt")
+
+;Seccion 1
+
 ;Int -> Int -> List
 ;Zones - Dado el ritmo cardiaco de descanso y el máximo ritmo cardiaco de una persona se debe regresar la lista de zonas de frecuencia cardiaca
 (define (zones rest max)
@@ -37,7 +40,59 @@
   (cond
     [(empty? mzones) empty]))
 
+;Seccion 2
 
+;ninBT - Dado un árbol de tipo BTree, determinar el número de nodos internos que tiene.
+
+(define (ninBT bt)
+  (cond
+    [(EmptyBT? bt) 0]
+    [(and (EmptyBT? (BNode-l bt)) (EmptyBT? (BNode-r bt))) 0]
+    [else (+ 1 (ninBT (BNode-l bt)) (ninBT (BNode-r bt)))]))
+
+;nlBT - Dado un árbol de tipo BTree, determinar el número de hojas no vacías.
+
+(define (nlBT bt)
+  (cond
+    [(EmptyBT? bt) 0]
+    [(and (EmptyBT? (BNode-l bt)) (EmptyBT? (BNode-r bt))) 1]
+    [else (+ (nlBT (BNode-l bt)) (nlBT (BNode-r bt)))]))
+
+;nnBT - Dado un árbol de tipo BTree, determinar el número de nodos que tiene. Las hojas vacías no cuentan.
+
+(define (nnBT bt)
+  (+ (ninBT bt) (nlBT bt)))
+
+;mapBT - Dado una función de aridad 1 y un árbol de tipo BTree, aplicar la función sobre todos los valores de
+;los nodos del árbol (las funciones de aridad 1 sólo regresas números).
+
+(define (mapBT f bt)
+  (cond
+    [(EmptyBT? bt) (EmptyBT)]
+    [else (bnn (mapBT f (BNode-l bt)) (f (BNode-e bt)) (mapBT f (BNode-r bt)))]))
+
+;preorderBT - Dado un árbol de tipo BTree, regresar una lista de sus elementos recorridos en preorden.
+
+(define (preorderBT bt)
+  (cond
+    [(EmptyBT? bt) '()]
+    [else (append (list (BNode-e bt)) (preorderBT (BNode-l bt)) (preorderBT (BNode-r bt)))]))
+
+;inorderBT - Dado un árbol de tipo BTree, regresar una lista de sus elementos recorridos en inorden.
+
+(define (inorderBT bt)
+  (cond
+    [(EmptyBT? bt) '()]
+    [else (append (inorderBT (BNode-l bt)) (list (BNode-e bt)) (inorderBT (BNode-r bt)))]))
+
+;posorderBT - Dado un árbol de tipo BTree, regresar una lista de sus elementos recorridos en post-orden.
+
+(define (posorderBT bt)
+  (cond
+    [(EmptyBT? bt) '()]
+    [else (append (posorderBT (BNode-l bt)) (posorderBT (BNode-r bt)) (list (BNode-e bt)))]))
+
+    
 ;Tests
 ;Zones
 (test (zones 50 180)(list
@@ -53,3 +108,26 @@
 (test (get-zone 'fat-burning my-zones) (fat-burning 128.0 140.0))
 (test (get-zone 'aerobic my-zones) (aerobic 141.0 153.0))
 (test (get-zone 'maximum my-zones) (maximum 167.0 180.0))
+;ninBT
+(test (ninBT (EmptyBT)) 0)
+(test (ninBT (BNode < (BNode < (EmptyBT) 3 (EmptyBT)) 1 (BNode < (EmptyBT) 2 (EmptyBT)))) 1)
+(test (ninBT arbol-base) 5)
+;nlBT
+(test (nlBT (EmptyBT)) 0)
+(test (nlBT (BNode < (BNode < (EmptyBT) 3 (EmptyBT)) 1 (BNode < (EmptyBT) 2 (EmptyBT)))) 2)
+(test (nlBT arbol-base) 4)
+;nnBT
+(test (nnBT (EmptyBT)) 0)
+(test (nnBT (BNode < (BNode < (EmptyBT) 3 (EmptyBT)) 1 (BNode < (EmptyBT) 2 (EmptyBT)))) 3)
+(test (nnBT arbol-base) 9)
+;mapBT
+(test (mapBT add1 (EmptyBT)) (EmptyBT))
+(test (mapBT add1 (BNode < (EmptyBT) 1 (BNode < (EmptyBT) 2 (EmptyBT)))) (BNode < (EmptyBT) 2 (BNode < (EmptyBT) 3 (EmptyBT))))
+(test (mapBT (lambda (x) (* x x)) (BNode < (EmptyBT) 3 (BNode < (EmptyBT) 2 (EmptyBT))))
+      (BNode < (EmptyBT) 9 (BNode < (EmptyBT) 4 (EmptyBT))))
+;preorderBT
+(test (preorderBT arbol-base) '("F" "B" "A" "D" "C" "E" "G" "I" "H"))
+;inorderBT
+(test (inorderBT arbol-base) '("A" "B" "C" "D" "E" "F" "G" "H" "I"))
+;posorderBT
+(test (posorderBT arbol-base) '("A" "C" "E" "D" "B" "H" "I" "G" "F"))
