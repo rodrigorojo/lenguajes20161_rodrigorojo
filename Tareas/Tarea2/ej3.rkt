46699a463dd2e6952935116124965bd1454c8742
JuanCarlos2 <juancarloslopez0607@gmail.com>
2015-10-08 21:58:10 -0500

    problema III tarea 2

---
+++ Tareas/Tarea2/ej3.rkt
@@ -0,0 +1,5 @@
+#lang plai
+
+(require "shriram-chapter8.rkt")
+
+(rinterp (cparse '{{fun {x} x}1}))
---
+++ Tareas/Tarea2/ej3doug.rkt
@@ -0,0 +1,5 @@
+#lang plai
+
+(require "shriram-chapter8-doug.rkt")
+
+(rinterp (cparse '{{fun {x} x}1}))
